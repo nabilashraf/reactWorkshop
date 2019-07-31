2e4477e09a33b8c1640f1a06b82bc86cb5ae01d6
Nabil Ashraf <nabeelashraf.2008@gmail.com>
2019-07-31 17:08:42 +0500

    small update

--- src/reducers/counterReducer.js
+++ src/reducers/counterReducer.js
@@ -9,9 +9,9 @@ const initial = {
 export default (state = initial, action) => {
   switch (action.type) {
     case types.INCREMENT_COUNTER:
-      return { ...state, counter: state.counter + 1 };
+      return { ...state, ...{counter: state.counter + 1} };
     case types.DECREMENT_COUNTER:
-      return { ...state, counter: state.counter - 1 };
+      return { ...state, ...{counter: state.counter - 1} };
     default:
       return state;
   }
--- src/router/config.js
+++ src/router/config.js
@@ -1,10 +1,8 @@
-import React from 'react';
 import { 
   createStackNavigator,
   createDrawerNavigator,
   createAppContainer
 } from 'react-navigation';
-import { Button } from 'react-native';
 import Home from '../container/Home';
 import Profile from '../container/Profile';
 
@@ -27,22 +25,7 @@ const AppDrawer = createDrawerNavigator({
   Profile: {
     screen: ProfileStack,
   }
-  },
-  {
-    initialRouteName: 'Home',
-    navigationOptions: navigationOptionsHeader
-  }
-);
-
-const navigationOptionsHeader = ({ navigation }) => {
-  return {
-    headerLeft: (
-      <Button onPress={() => navigation.openDrawer()} > 
-        Menu
-      </Button>
-    ),
-  };
-};
+});
 
 const RouteContainer = createAppContainer(AppDrawer);
 
